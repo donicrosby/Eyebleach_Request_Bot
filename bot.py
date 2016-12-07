38dea951e15ab0cb3cb67201604c202b9549618e
donicrosby <doni.crosby1995@gmail.com>
2016-12-07 15:21:31 +0000

    OAuth set up and working

--- bot.py
+++ bot.py
@@ -1,3 +1,20 @@
 import praw
-import simplejson as json
+import json
 
+# Opening the keys json file to read in sensitive script data
+with open('keys/keys.json') as key_data:
+    keys = json.load(key_data)
+
+# Assigning the bots secrets and client data
+bot_user_agent = keys["user_agent"]
+bot_client_id = keys["client_id"]
+bot_client_secret = keys["client_secret"]
+bot_password = keys["password"]
+
+bot_info = praw.Reddit(client_id = bot_client_id,
+                       client_secret = bot_client_secret,
+                       password = bot_password,
+                       user_agent = bot_user_agent,
+                       username = "EyebleachRequest_Bot")
+
+print(bot_info.user.me())
