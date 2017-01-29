2e2d3feb8274076b4820d47e2ed0edac05213e81
donicrosby <doni@HERMES.lan>
2017-01-28 19:01:10 -0500

    added subreddit parsing

--- bot.py
+++ bot.py
@@ -11,10 +11,20 @@ bot_client_id = keys["client_id"]
 bot_client_secret = keys["client_secret"]
 bot_password = keys["password"]
 
-bot_info = praw.Reddit(client_id = bot_client_id,
+reddit = praw.Reddit(client_id = bot_client_id,
                        client_secret = bot_client_secret,
                        password = bot_password,
                        user_agent = bot_user_agent,
                        username = "EyebleachRequest_Bot")
 
-print(bot_info.user.me())
+print "If this name:", reddit.user.me(), "is equal to the bot's username then the authentication was successful"
+
+# Retreving subreddits for the bot to use
+subreddits = reddit.subreddit('IrishJewTesting')
+
+# getting the cute subreddits
+aww = reddit.subreddit('aww')
+
+for submission in subreddits.hot(limit = 5)
+
+
